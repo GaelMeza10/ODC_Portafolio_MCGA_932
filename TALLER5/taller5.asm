a741f679d9473313f187e2eb11cd523eac9e02c1
GaelMeza <meza.gael@uabc.edu.mx>
2024-11-28 13:33:17 -0800

    taller5 update

--- TALLER5/taller5.asm
+++ TALLER5/taller5.asm
@@ -5,10 +5,8 @@ entrada
 section .bss
 input resb 1 ; Espacio para almacenar el carácter ingresado
 sum resb 1 ; Espacio para almacenar la suma
-
 section .text
-global _start
-
+  global _start
 _start:
 ; Mostrar mensaje en consola
 mov eax, 4
@@ -35,17 +33,12 @@ int 0x80 ; llamada al sistema
 ; Calcular la suma de los caracteres
 mov al, [input]
 add al, [input]
-mov [sum], al ; almacenar la suma en la variable sum
-
-; Mostrar la suma
+mov [sum], al ; almacenar la suma en la variable sum ; Mostrar la suma
 mov eax, 4
-
 mov ebx, 1
 mov ecx, sum ; dirección de la suma
 mov edx, 1 ; longitud de la suma
-int 0x80
-
-; Terminar el programa
+int 0x80 ; Terminar el programa
 mov eax, 1
 xor ebx, ebx ; código de salida 0
 int 0x80
